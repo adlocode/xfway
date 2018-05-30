1676c84c270d081cbd023bfd76eb29e62c5ffef9
adlo <adloconwy@gmail.com>
2018-05-30 04:17:02 +0100

    Add license

--- main.c
+++ main.c
@@ -1,3 +1,19 @@
+/* Copyright (C) 2018 adlo
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses/>
+ */
+
 #define _GNU_SOURCE
 #include <wayland-server.h>
 #include <compositor.h>
@@ -111,6 +127,7 @@ int main (int    argc,
     setenv ("WAYLAND_DISPLAY", socket_name, 1);
   }
 
+  weston_compositor_flush_heads_changed (ec);
 
   weston_compositor_wake (ec);
   wl_display_run (display);
