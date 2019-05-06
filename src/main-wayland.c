af29ed06e4dac2f93eee2d9f67d4e2115a56507c
adlo <adloconwy@gmail.com>
2019-05-07 00:33:13 +0100

    Update year to 2019

--- src/main-wayland.c
+++ src/main-wayland.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2018 adlo
+/* Copyright (C) 2018 - 2019 adlo
  *
  * This library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
